3aa73e13fe45ddce8b3f98f7ae45655ae04bc438
Peter <petergorman11@gmail.com>
2022-07-03 11:30:30 -0400

    Add IgnoreHashCodeComprarer for VariableContainer refactor

---
+++ Util/IgnoreHashCodeComparer.cs
@@ -0,0 +1,14 @@
+﻿namespace SandScript;
+
+public class IgnoreHashCodeComparer<T> : IEqualityComparer<T>
+{
+	public bool Equals( T? x, T? y )
+	{
+		if ( x is null )
+			return y is null;
+
+		return x.Equals( y );
+	}
+
+	public int GetHashCode( T obj ) => 0;
+}
