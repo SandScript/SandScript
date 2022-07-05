4d550b97eb22b46d3b0a6a0cca549a04beba1b2e
Peter <petergorman11@gmail.com>
2022-07-05 17:22:46 -0400

    Refactor Script

--- Script/Script.cs
+++ Script/Script.cs
@@ -1,65 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Immutable;
 using SandScript.Exceptions;
 
 namespace SandScript;
 
 public class Script
 {
-	public IReadOnlyDictionary<string, ScriptValue> Globals
+	public ImmutableDictionary<string, ScriptValue> Globals
 	{
 		get
 		{
-			if ( !TryGetStage<Interpreter>( out var interpreter ) )
-				throw new StageMissingException( typeof(Interpreter) );
-
-			var globals = new Dictionary<string, ScriptValue>();
-			foreach ( var rawGlobal in interpreter.Variables.Root )
+			var globals = ImmutableDictionary.CreateBuilder<string, ScriptValue>();
+			foreach ( var rawGlobal in Interpreter.Variables.Root )
 				globals.Add( rawGlobal.Key, ScriptValue.From( rawGlobal.Value ) );
-			return globals;
-		}
-	}
-	
-	public string LastText { get; private set; }
-	
-	private readonly List<IStage> _stages = new();
-	private readonly Dictionary<Type, object?[]> _stageResults = new();
-
-	private Script()
-	{
-	}
-
+			return globals.ToImmutable();
 		}
 	}
 
+	protected readonly SemanticAnalyzer Analyzer = new();
+	protected readonly Interpreter Interpreter = new();
 
 	public void AddGlobal( string varName, ScriptValue value )
 	{
-
-		if ( analyzer.VariableTypes.Root.ContainsKey( varName ) )
+		if ( Analyzer.VariableTypes.Root.ContainsKey( varName ) )
 			throw new GlobalRedefinedException( varName );
 
 		var valueTypeProvider = TypeProviders.GetByType( value.Type );
 		if ( valueTypeProvider is null )
 			throw new TypeUnsupportedException( value.Type );
 		
-		analyzer.VariableTypes.Root.Add( varName, valueTypeProvider );;
-		interpreter.Variables.Root.Add( varName, value );
+		Analyzer.VariableTypes.Root.Add( varName, valueTypeProvider );;
+		Interpreter.Variables.Root.Add( varName, value );
 
 		if ( valueTypeProvider != TypeProviders.Builtin.Method )
 			return;
 
 		var method = (ScriptMethod)value.Value!;
 		var methodSignature = MethodSignature.From( method );
-		analyzer.VariableMethods.Root.Add( methodSignature, method );
-		interpreter.MethodVariables.Root.Add( methodSignature, value );
+		Analyzer.VariableMethods.Root.Add( methodSignature, method );
+		Interpreter.MethodVariables.Root.Add( methodSignature, value );
 	}
 
 	public ScriptValue Call( ScriptMethod method, params object?[] args )
 	{
-
-		return ScriptValue.From( method.Invoke( interpreter, args ) );
+		return ScriptValue.From( method.Invoke( Interpreter, args ) );
 	}
 
 	public ScriptValue Call( ScriptValue sv, params object?[] args )
@@ -67,34 +50,42 @@ public class Script
 		if ( sv.TypeProvider != TypeProviders.Builtin.Method )
 			throw new TypeMismatchException( TypeProviders.Builtin.Method, sv.TypeProvider );
 
-		return Call( (ScriptMethod)sv.Value!, args );
-	}
-
+		return ScriptValue.From( ((ScriptMethod)sv.Value!).Invoke( Interpreter, args ) );
 	}
 
 	public ScriptValue? Execute( string text ) => Execute( text, out ScriptDiagnostics _ );
 	public ScriptValue? Execute( string text, out ScriptDiagnostics diagnostics )
 	{
+		diagnostics = new ScriptDiagnostics();
+		
+		var tokens = Lexer.Lex( text, false, out var lexerDiagnostics );
+		diagnostics.AddStageAndClear( lexerDiagnostics );
+		
+		var programAst = Parser.Parse( tokens, out var parserDiagnostics );
+		diagnostics.AddStageAndClear( parserDiagnostics );
+		
+		var optimizedProgramAst = Optimizer.Optimize( programAst, out var optimizerDiagnostics );
+		diagnostics.AddStageAndClear( optimizerDiagnostics );
 
-		var results = Run( text, false, out diagnostics );
-		if ( results.Length == 0 )
+		var analysisResult = Analyzer.AnalyzeAst( optimizedProgramAst );
+		diagnostics.AddStageAndClear( Analyzer.Diagnostics );
+		if ( !analysisResult )
 			return null;
 
-		if ( results[0] is ScriptValue sv )
+		var result = Interpreter.Interpret( optimizedProgramAst );
+		diagnostics.AddStageAndClear( Interpreter.Diagnostics );
+		
+		if ( result is ScriptValue sv )
 			return sv;
 		
-		return ScriptValue.From( results[0] );
-	}
-
-	public static Script Create()
-	{
-		return new Script();
+		return ScriptValue.From( result );
 	}
 
 	public static Script Execute( string text, out ScriptValue? returnValue ) =>
 		Execute( text, out returnValue, out _ );
 	public static Script Execute( string text, out ScriptValue? returnValue, out ScriptDiagnostics diagnostics )
 	{
+		var script = new Script();
 		returnValue = script.Execute( text, out diagnostics );
 		return script;
 	}
